11bbd38c62a8f9c2fe49278ad441310e30f02bbc
Frixar118 <ricardo.misraim@gmail.com>
2025-11-16 14:21:01 -0600

    act

--- pagina/models.py
+++ pagina/models.py
@@ -4,6 +4,7 @@ class Producto(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
     precio = models.DecimalField(max_digits=6, decimal_places=2)
+    imagen = models.CharField(max_length=200, blank=True, null=True)
 
     def __str__(self):
         return self.nombre
